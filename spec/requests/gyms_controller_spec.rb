9541a9f159892f8aa8d68560ed4f03f4fc5824d4
profadevairvitorio <profadevairvitorio@gmail.com>
2023-09-03 14:16:30 -0300

    task: add spec to /api/v1/gyms

--- spec/requests/gyms_controller_spec.rb
+++ spec/requests/gyms_controller_spec.rb
@@ -3,4 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Gyms', type: :request do
+  let!(:gyms) { create_list(:gym, 3) }
+
+  describe 'GET /api/v1/gyms' do
+    before { get '/api/v1/gyms' }
+
+    it 'returns a list of gyms' do
+      expect(JSON(response.body)).not_to be_empty
+      expect(JSON(response.body).count).to eq(3)
+    end
+
+    it 'returns a status code :ok' do
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
