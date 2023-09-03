2576ce041b1bd331726b46b9b70f35932b7aad3b
profadevairvitorio <profadevairvitorio@gmail.com>
2023-09-03 14:41:35 -0300

    task: add spec to GET  /api/v1/gyms:id

--- spec/requests/gyms_controller_spec.rb
+++ spec/requests/gyms_controller_spec.rb
@@ -3,10 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Gyms', type: :request do
-  let!(:gyms) { create_list(:gym, 3) }
-  let(:gym_id) { gyms.first.id }
-
   describe 'GET /api/v1/gyms' do
+    let!(:gyms) { create_list(:gym, 3) }
+    let(:gym_id) { gyms.first.id }
+
     before { get '/api/v1/gyms' }
 
     it 'returns a list of gyms' do
@@ -20,12 +20,16 @@ RSpec.describe 'Gyms', type: :request do
   end
 
   describe 'GET /api/v1/gyms/:id' do
+    let!(:gym) { create(:gym, name: 'Vitorio GYM') }
+    let(:gym_id) { gym.id }
+
     before { get "/api/v1/gyms/#{gym_id}" }
 
     context 'when the gym exists' do
       it 'returns the gym' do
         expect(JSON(response.body)).not_to be_empty
         expect(JSON(response.body)['id']).to eq(gym_id)
+        expect(JSON(response.body)['name']).to eq('Vitorio GYM')
       end
       it 'returns a status code :ok' do
         expect(response).to have_http_status(:ok)
@@ -44,4 +48,6 @@ RSpec.describe 'Gyms', type: :request do
       end
     end
   end
+
+
 end
