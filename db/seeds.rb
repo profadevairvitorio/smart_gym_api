03bf8051c427b46d964fb3355a1a0bf2740519d1
Prof Adevair Vitório <121971339+profadevairvitorio@users.noreply.github.com>
2023-09-07 21:28:39 -0300

    Atualizar o seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+require 'factory_bot_rails'
+
+# Crie 10 ginásios usando a factory
+10.times do
+  FactoryBot.create(:gym)
+end
+
+puts "Academias cadastradas com sucesso!"
